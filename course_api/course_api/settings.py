c929b250b57fed84328add4d6267de17e994d691
4i4ek <4i4ekov@gmail.com>
2021-12-12 23:19:12 +0300

    add files 2

--- course_api/course_api/settings.py
+++ course_api/course_api/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g!d$1a*7&n=)9^0d-2w8s4mho&r%(sew(b@#vu#%aulhgby1$q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
